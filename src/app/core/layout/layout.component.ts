bd907c85fc8a5f1fd854681f0f19de13896bd5e4
Thomas Artois <thomas_artois99@hotmail.com>
2024-01-09 12:04:35 +0100

    Added header & footer

---
+++ src/app/core/layout/layout.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
+import {RouterOutlet} from "@angular/router";
+import {HeaderComponent} from "./header/header.component";
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    FooterComponent,
+    RouterOutlet,
+    HeaderComponent
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
