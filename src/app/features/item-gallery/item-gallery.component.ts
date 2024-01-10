05e2911b91ce6b3a33c04240778ac2ce06fb6063
Thomas Artois <thomas_artois99@hotmail.com>
2024-01-10 10:54:37 +0100

    item filter done

--- src/app/features/item-gallery/item-gallery.component.ts
+++ src/app/features/item-gallery/item-gallery.component.ts
@@ -2,16 +2,19 @@ import {Component, OnInit} from '@angular/core';
 import {Item} from "../../core/model/Item";
 import {ItemService} from "../../core/service/item/item.service";
 import {CommonModule} from "@angular/common";
+import {NameFilterPipe} from "../../core/pipes/name-filter.pipe";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-item-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NameFilterPipe, FormsModule],
   templateUrl: './item-gallery.component.html',
   styleUrl: './item-gallery.component.css'
 })
 export class ItemGalleryComponent implements OnInit {
   items: Item[] = [];
+  searchText: string = "";
 
   constructor(private itemService: ItemService) {
   }
